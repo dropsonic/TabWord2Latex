d0bd3ecb993587a8db4612841a30a8f66143e6f8
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-11-17 14:49:03 +0400

    Word Cells cycle

--- TabWord2Latex/WordParser.cs
+++ TabWord2Latex/WordParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,22 @@ namespace TabWord2Latex
             //    columns.Add(new Column() { Width = wordColumn.Width });
 
             //table.Columns = columns;
+
+            int totalCols = wordTable.Columns.Count;
+            int totalRows = wordTable.Rows.Count;
+            for (int i = 1; i <= totalCols; i++)
+                for (int j = 1; j <= totalRows; j++)
+                {
+                    try
+                    {
+                        Word.Cell cell = wordTable.Cell(j, i);
+                        Console.WriteLine("{0} col, {1} row: {2}", i, j, cell.Range.Text);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine("NO: {0} col, {1} row", i, j);
+                    }
+                }
 
             return table;
         }

dcf09ae6a4251ab9f9ef6233c711382f4a90e085
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-11-18 13:28:31 +0400

    Assembly references

--- TabWord2Latex/Program.cs
+++ TabWord2Latex/Program.cs
@@ -40,7 +40,7 @@ namespace TabWord2Latex
                             throw new ApplicationException("Document does not contain a table.");
 
                         if (options.TableNumber > tablesCount)
-                            throw new ApplicationException(String.Format("Table number is too big. Document contains only {0} tables.", document.Tables.Count));
+                            throw new ApplicationException(String.Format("Table number is too big. Document contains only {0} tables.", tablesCount));
 
                         var table = tables.ElementAt(options.TableNumber - 1);
 
